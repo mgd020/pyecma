6698fae683ba5e2283c59cbe06a4fc46130f43f0
Matthew <mgd020@gmail.com>
2019-07-18 14:46:38 +1000

    Handle more ast nodes

--- src/ecma/parser.py
+++ src/ecma/parser.py
@@ -25,6 +25,11 @@ def parse_script(code):
 
 
 class _Parser:
+    Number = ast.Name(id="Number", ctx=ast.Load())
+    String = ast.Name(id="String", ctx=ast.Load())
+    typeof = ast.Name(id="typeof", ctx=ast.Load())
+    strictly_equal = ast.Name(id="strictly_equal", ctx=ast.Load())
+
     def __call__(self, node, metadata):
         # esprima.Parser will call this as the delegate
         node.ast = self._parse(node)
@@ -34,14 +39,108 @@ class _Parser:
         try:
             parser = getattr(self, "_%s" % node.type)
         except AttributeError:
-            self._unhandled(node)
-        return parser(node)
+            pass
+        else:
+            py_ast = parser(node)
+            if py_ast is not None:
+                print("OLD")
+                print(node)
+                print("")
+                print("NEW")
+                print(astunparse.unparse(py_ast))
+                return py_ast
 
-    def _unhandled(self, node):
         raise NotImplementedError(
             "Node type not implemented: (line %d col %d)\n%r"
             % (node.loc.start.line, node.loc.start.column, node)
         )
+
+    def _Literal(self, node):
+        if isinstance(node.value, float):
+            return ast.Call(func=self.Number, args=[ast.Num(n=node.value)], keywords=[])
+        if isinstance(node.value, str):
+            return ast.Call(func=self.String, args=[ast.Str(s=node.value)], keywords=[])
+
+    def _ExpressionStatement(self, node):
+        return self._parse(node.expression)
+
+    def _Identifier(self, node):
+        return ast.Name(id=node.name.replace("$", "_dollar_"), ctx=ast.Load())
+
+    UNARY_OPERATOR = {"!": ast.Not()}
+
+    def _UnaryExpression(self, node):
+        if node.operator == "typeof":
+            return ast.Call(func=self.typeof, args=[node.argument.ast], keywords=[])
+        try:
+            op_ast = self.UNARY_OPERATOR[node.operator]
+        except KeyError:
+            pass
+        else:
+            return ast.UnaryOp(op=op_ast, operand=node.argument.ast)
+
+    BINARY_OPERATORS = {"+": ast.Add(), "-": ast.Sub(), "*": ast.Mult(), "/": ast.Div()}
+
+    COMPARE_OPERATORS = {
+        "==": ast.Eq(),
+        "<": ast.Lt(),
+        ">": ast.Gt(),
+        "<=": ast.LtE(),
+        ">=": ast.GtE(),
+        "!=": ast.NotEq(),
+    }
+
+    def _BinaryExpression(self, node):
+        # strictly_equal
+        if node.operator in ("===", "!=="):
+            py = ast.Call(
+                func=self.strictly_equal, args=[node.left.ast, node.right.ast], keywords=[]
+            )
+            if node.operator[0] == "!":
+                py = ast.UnaryOp(op=ast.Not(), operand=py)
+            return py
+
+        # binary ops
+        try:
+            op_ast = self.BINARY_OPERATORS[node.operator]
+        except KeyError:
+            pass
+        else:
+            return ast.BinOp(left=node.left.ast, op=op, right=node.right.ast)
+
+        # compare
+        try:
+            op_ast = self.COMPARE_OPERATORS[node.operator]
+        except KeyError:
+            pass
+        else:
+            return ast.Compare(left=node.left.ast, ops=[op], comparators=[node.right.ast])
+
+    def _MemberExpression(self, node):
+        if node.computed:
+            return ast.Subscript(
+                value=node.object.ast,
+                slice=ast.Index(value=node.property.ast, ctx=ast.Load()),
+                ctx=ast.Load(),
+            )
+        if node.property.type == "Identifier":
+            return ast.Attribute(value=node.object.ast, attr=node.property.name, ctx=ast.Load())
+
+    BOOLEAN_OPERATORS = {"&&": ast.And(), "||": ast.Or()}
+
+    def _LogicalExpression(self, node):
+        try:
+            op_ast = self.BOOLEAN_OPERATORS[node.operator]
+        except KeyError:
+            pass
+        else:
+            return ast.BoolOp(op=op_ast, values=[node.left.ast, node.right.ast])
+
+    def _ReturnStatement(self, node):
+        return ast.Return(value=node.argument.ast)
+
+    def _BlockStatement(self, node):
+        return [n.ast for n in node.body]
 
 
 if __name__ == "__main__":
